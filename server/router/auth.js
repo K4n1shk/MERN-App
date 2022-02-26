e6c00005aa4b8d810dc63a6aeb325238674f06d0
K4n1shk <k4n1shk1501@gmail.com>
2022-02-26 21:07:22 +0530

    Promise Version

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,8 +9,28 @@ router.get("/", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+  const { name, email, phone, work, password, cpassword } = req.body;
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Plz fill the field properly" });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "Email already exist" });
+      }
+
+      const user = new User({ name, email, phone, work, password, cpassword });
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "User registered successfully!" });
+        })
+        .catch((err) => res.status(500).json({ error: "Failed to register!" }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
