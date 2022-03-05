7613d821ea5dc47cdd13c06b8ce020bb3ea1f072
K4n1shk <k4n1shk1501@gmail.com>
2022-03-05 09:12:49 +0530

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 const App = () => {
-  return <div>Hello from react!</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
